3cba702892f3d8994e728f374bc614e80d28dbc8
Flora Trecul <flora.trecul@gmail.com>
2025-03-25 17:41:37 +0100

    Added priceRange and image for Rich Snippets + removed role=document in modal to solve an accessibility conflict + added lazy loading on all images below carousel

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -210,7 +210,7 @@
       gallery.append(`<div class="modal fade" id="${
         lightboxId ? lightboxId : "galleryLightbox"
       }" tabindex="-1" role="dialog" aria-hidden="true">
-                <div class="modal-dialog" role="document">
+                <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-body">
                             ${
